a4bee2b9f4877e5070ff40944f211aa6bc57622d
Andre Hilario <andre18hilario@gmail.com>
2023-05-18 14:24:37 -0300

    feat: Create GET ranking on path "/ranking"

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -7,15 +7,16 @@ export async function getUsers(req, res) {
 
     try {
 
-        const userInfos = await db.query(`
-        SELECT users.id AS user_id, users.name AS user_name, 
-               urls.id AS url_id, urls."shortUrl", urls.url, COUNT(urls."visitCount") AS url_visit_count
-        FROM urls
-        JOIN users 
-            ON urls."userId" = users.id
-        WHERE users.id = $1
-        GROUP BY users.id, users.name, urls.id;
-      `, [userId]);
+        const userInfos = await db.query(
+            `
+                SELECT users.id AS user_id, users.name AS user_name, 
+                    urls.id AS url_id, urls."shortUrl", urls.url, SUM(urls."visitCount") AS url_visit_count
+                FROM urls
+                JOIN users 
+                    ON urls."userId" = users.id
+                WHERE users.id = $1
+                GROUP BY users.id, users.name, urls.id;
+            `, [userId]);
 
         const correctUserInfos = {
             id: userInfos.rows[0].user_id,
@@ -30,12 +31,44 @@ export async function getUsers(req, res) {
                 id: row.url_id,
                 shortUrl: row.shortUrl,
                 url: row.url,
-                visitCount: row.url_visit_count
+                visitCount: Number(row.url_visit_count)
             });
         });
 
-        
+
         res.status(200).send(correctUserInfos);
+
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+}
+
+export async function getRanking(req, res) {
+
+    try {
+
+        const ranking = await db.query(
+            `
+                SELECT users.id AS id, users.name AS name, 
+                    COUNT(urls.id) AS "linksCount",
+                    COALESCE(SUM(urls."visitCount"), 0) AS "visitCount"
+                FROM users
+                LEFT JOIN urls
+                    ON urls."userId" = users.id
+                GROUP BY users.id, users.name
+                ORDER BY "visitCount" DESC
+                LIMIT 10;
+            `
+        );
+        console.log(ranking)
+        const rankingResponse = ranking.rows.map((row) => ({
+            id: row.id,
+            name: row.name,
+            linksCount: Number(row.linksCount),
+            visitCount: Number(row.visitCount)
+        }));
+
+        res.status(200).send(rankingResponse);
 
     } catch (err) {
         res.status(500).send(err.message);
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { getUsers } from "../controllers/users.controllers.js";
+import { getRanking, getUsers } from "../controllers/users.controllers.js";
 import { authValidation } from "../middlewares/auth.middleware.js";
 
 
 const userRouter = Router();
 
 userRouter.get("/users/me", authValidation, getUsers);
+userRouter.get("/ranking", getRanking);
 
 export default userRouter;
