fd6e01f77a8f9b6e6db1f00014207c772563c0ee
Andre Hilario <andre18hilario@gmail.com>
2023-05-18 15:57:41 -0300

    fix: password length

--- src/middlewares/url.middleware.js
+++ src/middlewares/url.middleware.js
@@ -5,14 +5,14 @@ export async function validateShortUrl(req, res, next) {
     const { id, shortUrl } = req.params;
 
     if (id) {
-        const getUrl = await db.query(`SELECT * FROM urls WHERE id=$1;`, [id]);
+        const getUrl = await db.query(`SELECT * FROM urls WHERE id = $1;`, [id]);
         if (getUrl.rowCount === 0) return res.sendStatus(404);
 
         res.locals.getUrl = getUrl.rows[0];
     }
 
     if (shortUrl) {
-        const getUrl = await db.query(`SELECT * FROM urls WHERE "shortUrl"=$1;`, [shortUrl]);
+        const getUrl = await db.query(`SELECT * FROM urls WHERE "shortUrl" = $1;`, [shortUrl]);
         if (getUrl.rowCount === 0) return res.sendStatus(404);
 
         res.locals.getUrl = getUrl.rows[0];
--- src/schemas/signup.schema.js
+++ src/schemas/signup.schema.js
@@ -3,7 +3,10 @@ import Joi from "joi";
 export const signupSchema = Joi.object({
     name: Joi.string().trim().required(),
     email: Joi.string().trim().email().required(),
-    password: Joi.string().trim().required(),
+    password: Joi.string().trim().min(4).required().messages({
+        'string.min': 'A senha deve ter no mínimo 4 caracteres.',
+        'any.required': 'A senha é obrigatória.',
+    }),
     confirmPassword: Joi.string().trim().valid(Joi.ref('password')).required().messages({
         'any.only': 'A confirmação da senha deve ser igual à senha escolhida.',
         'any.required': 'A confirmação da senha é obrigatória.',
